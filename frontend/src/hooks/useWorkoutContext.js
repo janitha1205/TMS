e4042c4282f5fe975953d9bd8dcaf7384646b2f9
janitha thalagoda <112193392+janitha1205@users.noreply.github.com>
2024-04-09 11:06:12 +0530

    Add files via upload

--- frontend/src/hooks/useWorkoutContext.js
+++ frontend/src/hooks/useWorkoutContext.js
@@ -1,1 +1,18 @@
+import {WorkoutContext} from '../context/WorkoutContext'
+import { useContext } from 'react'
+
+export const useWorkoutContext = () => {
+
+ 
+    const context =useContext(WorkoutContext)
+    if(!context){
+        throw Error('useworkoutcontext must be used inside an workout context provider')
+    }
+
+
+
+
+
+    return context
+}
 
